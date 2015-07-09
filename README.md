144f4fc9904870aaa794898d61f13650f3625818
Guillaume Fay <guillaume_fay@hotmail.com>
2015-07-09 15:51:35 +0200

    Add configuration file conf.ini for GSHcreator and a CRON script update-grouper.sh

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Ensemble de scripts permettant de récupérer les groupes présents dans l'annua
 
 Il s'agit d'un fork du projet esup-tools initialement versionné sur SVN (https://sourcesup.renater.fr/projects/esup-tools/)
 
+Configuration : Copier le fichier config.dist.ini en config.ini et remplir les champs.
+
 GSHcreator.py : Génère un fichier GSH qui permet d'exporter les groupes LDAP dans Grouper. 
 
 PAGScreator : Génère un fichier XML qui permet d'importer les groupes LDAP dans ESUP-Portal.
---
+++ update-grouper.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+# Script à exécuter dans un CRON une fois par jour.
+
+GROUPER_HOME=/usr/local/grouper/grouper
+GROUPER_LOG=/var/log/grouper
+
+curDir=$(dirname $0)
+
+out_file=$curDir/temp.gsh
+conf_file=$curDir/GSHcreator.ini
+log_file=$GROUPER_LOG/GSHcreator.log.
+
+echo -e "Creating GSH temporary file...\n"
+$curDir/GSHcreator.py $conf_file $out_file $log_file
+
+echo -e "Execute GSH temporary file...\n"
+$GROUPER_HOME/bin/gsh $out >> $GROUPER_LOG/GSHcreator-gsh.log
+
+# ensure modifications go to LDAP (for modifications not handled by export-modified-groups-to-LDAP)
+echo -e "Ensure modifications go to LDAP...\n"
+$GROUPER_HOME/bin/gsh -psp -bulkSync >> $GROUPER_LOG/GSHcreator-psp-bulkSync.log
